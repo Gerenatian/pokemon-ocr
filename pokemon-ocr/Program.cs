2ef6512b2f7d7d976583a52da81ca9ecf23f8899
Gerenatian <ben_green27@yahoo.com>
2020-01-27 00:05:54 -0500

    ocr begginings

---
+++ pokemon-ocr/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace pokemon_ocr
+{
+    class Program
+    {
+        public static string strRegex = @"[0-9]+\/([0-9]+)";
+
+        static void Main(string[] args)
+        {
+            var Ocr = new IronOcr.AutoOcr();
+            var Result = Ocr.Read(@"C:\workspaces\pokemon-ocr\pokemon-ocr\tests\test.jpg");
+            Console.WriteLine(Result.Text);
+
+
+            Regex myRegex = new Regex(strRegex);
+            Match myMatch = myRegex.Match(Result.Text);
+
+
+            Console.WriteLine(myMatch);
+
+            Console.Write("yo");
+        }
+    }
+}
+
+//
